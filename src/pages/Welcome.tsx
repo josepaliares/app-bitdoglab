c7a73ff46e73cfef2cf9a8d67b144520cbb03d37
Jose Paliares <josearthurpaliares@gmail.com>
2025-06-05 15:12:29 -0300

    add disclaimer

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,28 +1,44 @@
-import { Button } from '@/components/ui/button';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
+import { Dialog } from "@capacitor/dialog";
 
 export default function Welcome() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className='bg-gradient-to-bl from-pink-30 to-blue-30 h-screen flex flex-col items-center justify-center gap-6 px-4'>
-            <h2 className='text-ubuntu font-bold text-lg px-4 py-2 text-center text-white'>
-                Olá! Seja bem vindo ao
-            </h2>
-            
-            <h1 className='text-ubuntu text-5xl font-bold text-center'>
-                <span className='text-blue-500'>Bit</span>
-                <span className='text-pink-500'>Dog</span>
-                <span className='text-black'>Lab</span>
-            </h1>
-            
-            <h2 className='text-ubuntu font-medium text-lg px-4 py-2 text-center text-white'>
-                Venha traçar uma jornada de aprendizado e de criatividade
-            </h2>
-            
-            <Button onClick={() => navigate('/connection')} className='mt-4'>
-                Começar
-            </Button>
-        </div>
-    );
+  const hasShown = useRef(false);
+  useEffect(() => {
+    if (!hasShown.current) {
+      hasShown.current = true;
+      Dialog.alert({
+        title: "Aviso - Versão Antecipada",
+        message: `Este aplicativo está em fase de desenvolvimento.
+Algumas funcionalidades podem não funcionar corretamente.
+
+Agradecemos sua compreensão!`,
+      });
+    }
+  }, []);
+
+  return (
+    <div className="bg-gradient-to-bl from-pink-30 to-blue-30 h-screen flex flex-col items-center justify-center gap-6 px-4">
+      <h2 className="text-ubuntu font-bold text-lg px-4 py-2 text-center text-white">
+        Olá! Seja bem vindo ao
+      </h2>
+
+      <h1 className="text-ubuntu text-5xl font-bold text-center">
+        <span className="text-blue-500">Bit</span>
+        <span className="text-pink-500">Dog</span>
+        <span className="text-black">Lab</span>
+      </h1>
+
+      <h2 className="text-ubuntu font-medium text-lg px-4 py-2 text-center text-white">
+        Venha traçar uma jornada de aprendizado e de criatividade
+      </h2>
+
+      <Button onClick={() => navigate("/connection")} className="mt-4">
+        Começar
+      </Button>
+    </div>
+  );
 }
