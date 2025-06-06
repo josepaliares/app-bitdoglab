92e2f17ee1b87a2d9f2fc19764c8f8f017745168
Wallysson Oliveira <wallyssonryu@gmail.com>
2025-06-06 17:23:59 -0300

    FIX: Better architeture

--- src/json/toMicropython.ts
+++ src/json/toMicropython.ts
@@ -1,3 +1,5 @@
+import { interpreterNeopixel } from "./interpreters/neoPixel"
+import { interpreterLedRGB } from "./interpreters/ledRGB"
 export { toMicropython }
 
 function parse(json: string): [string, unknown] {
@@ -21,40 +23,4 @@ function micropython(app: string, instructions: unknown): string[] {
 function toMicropython(json: string): string[] {
 	const params: [string, unknown] = parse(json);
 	return micropython(params[0], params[1]);
-}
-
-function interpreterNeopixel(instructions: { pos: string, cor: string }[]): string[] {
-	function transformNumber(num: number): number {
-		const swapMap: Record<number, number> = {
-			0: 4, 4: 0,
-			1: 3, 3: 1,
-			10: 14, 14: 10,
-			11: 13, 13: 11,
-			20: 24, 24: 20,
-			21: 23, 23: 21
-		};
-
-		return swapMap[num] ?? num;
-	}
-	const res: string[] = [];
-	instructions.forEach(dict => {
-		const rgbMatch: string[] = dict.cor.match(/\d+/g) as string[];
-		const rgb: string = rgbMatch.join(', ');
-		const pos: number = transformNumber(parseInt(dict.pos));
-		res.push(`np[` + pos + `] = (${rgb})`);
-	});
-	res.push(`np.write()`);
-	return res;
-}
-
-function interpreterLedRGB(instructions: string): string[] {
-	const res: string[] = [];
-	const rgbMatch: string[] = instructions.match(/\d+/g) as string[];
-	const corR: number = parseInt(rgbMatch.at(0) as string) << 8;
-	const corG: number = parseInt(rgbMatch.at(1) as string) << 8;
-	const corB: number = parseInt(rgbMatch.at(2) as string) << 8;
-	res.push(`pwmR.duty_u16(${corR})`);
-	res.push(`pwmG.duty_u16(${corG})`);
-	res.push(`pwmB.duty_u16(${corB})`);
-	return res;
 }
