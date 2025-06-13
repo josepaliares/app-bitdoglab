8355db501888abe4cedaac01f1ac8ce2038bdd2c
Jose Paliares <josearthurpaliares@gmail.com>
2025-06-13 11:10:06 -0300

    corrige comandos enviados ao buzzer

--- src/json/interpreters/buzzer.ts
+++ src/json/interpreters/buzzer.ts
@@ -6,15 +6,15 @@ export function interpreterBuzzer(data: BuzzersData): string[] {
   if (data.isPressed && data.frequency) {
     
     commands.push(`buzzer.freq(${data.frequency})`);
-    commands.push(`buzzer.duty(700)`);
-    commands.push(`buzzerAux.duty(300)`);
+    commands.push(`buzzer.duty_u16(700)`);
+    commands.push(`buzzerAux.duty_u16(300)`);
     commands.push(`print('Buzzers ligados - Frequência: ${data.frequency}Hz')`);
     
   } 
   else if (!data.isPressed) {
 
-    commands.push(`buzzer.duty(0)`);
-    commands.push(`buzzerAux.duty(0)`);
+    commands.push(`buzzer.duty_u16(0)`);
+    commands.push(`buzzerAux.duty_u16(0)`);
     
     if (data.duration) {
       commands.push(`print('Buzzer desligado - Duração: ${data.duration}ms')`);
